9ce9d4a943feebd145beb6e787be8ed5db7f5237
Giancarlos Cercado <gianmaster@users.noreply.github.com>
2021-04-15 16:05:55 -0500

    Added proxy server to github enpoint

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,7 @@
 const environment = {
   production: false,
-  apiUrl: 'https://jobs.github.com/positions',
+  apiUrl: 'https://stark-mesa-12909.herokuapp.com/https://jobs.github.com/positions',
+  // apiUrl: 'https://jobs.github.com/positions',
   // apiUrl: '/positions' // this works for localhost, it uses a proxy configured on packege.json
 };
 
